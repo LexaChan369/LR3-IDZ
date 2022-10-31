4c8f41d2f7a7d516489f5b4c04dd45567e2eb432
yaolg <yaolg@DESKTOP-PS4EPC5>
2022-10-31 20:48:57 +0300

    Добавьте файлы проекта.

---
+++ ЛР3_ИДЗ/Program.cs
@@ -0,0 +1,15 @@
+﻿using LIB;
+namespace ЛР3_ИДЗ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Matrix matrix = new Matrix(2, 2);
+            Matrix.PrintMatrix(matrix);
+            Console.WriteLine(Matrix.GetSrArif(matrix));
+            Console.WriteLine();
+            Matrix.PrintResultMatrix(matrix);
+        }
+    }
+}
